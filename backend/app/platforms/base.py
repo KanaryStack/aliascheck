48169a131b53bbae19941c2c013666dc87cc7dd9
daniel.kiptoon@outlook.com <daniel.kiptoon@outlook.com>
2022-08-03 21:10:46 +0300

    add base twitter client

---
+++ backend/app/platforms/base.py
@@ -0,0 +1,8 @@
+import abc
+
+
+from app.platforms.response import AliasCheckResponse
+class BasePlatformClient():
+    @abc.abstractmethod
+    def check_alias(self,alias:str)->AliasCheckResponse:
+        pass
