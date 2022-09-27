29888e421d004686d8ea8eb7c29068532fb58406
rwambui <rosemwangi305@gmail.com>
2022-09-27 11:43:03 +0300

    nav styling updated

--- client/src/components/Navbar/styles.ts
+++ client/src/components/Navbar/styles.ts
@@ -5,32 +5,37 @@ export default makeStyles((theme: Theme) => ({
   container: {
     display: "flex",
     alignItems: "center",
-    width: "100%",   
-    maxWidth: "1200px",
-    margin: "auto", 
+    width: "100%",
+    maxWidth: "1300px",
+    margin: "auto",
+    paddingTop: "1rem",
   },
   title: {
-    fontWeight: 700,
-    fontSize: "2.5rem", 
-    color: "#031521", 
-    flexGrow: 1, 
-    alignItems: "center",
-    display: "flex",
+    fontWeight: 700,  
+    fontSize: "2.5rem",
+    alignItems: "center", 
     margin: "0rem !important", 
 
     [theme.breakpoints.up('md')]: {
-      display: "inline-block",
-      color: "#000", 
+      display: "inline-block",  
       fontWeight: "bold",
       margin: "3rem 1.2rem 0",
     },
 
     [theme.breakpoints.up('lg')]: {
-      marginLeft: "3rem",
+      marginLeft: "3rem", 
     },
   },
   grow: {
     flexGrow: 1,
-  },
+  }, 
   menuItems: {},
+  button: {
+    "&:hover": {
+      transform: "scale(0.9)",
+    },         
+  },
+  // btn: {
+  //   color: "#fff",
+  // }
 }));
