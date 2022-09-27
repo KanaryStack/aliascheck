a1376df114868bc5cb22603149d6d94b6425380e
rwambui <rosemwangi305@gmail.com>
2022-09-27 11:39:54 +0300

    minor nav display changes

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,8 +1,8 @@
 import useStyles from "./styles";
 import { Toolbar, Button, Typography, Box } from "@mui/material";
-
+import Switcher from "../Switcher";
 interface Props {
-  window?: () => Window; 
+  window?: () => Window;
 }
 const menuItems = ["Services", "FAQs"];
 
@@ -12,28 +12,40 @@ const Navbar: React.FC = () => {
     <>
       <Box sx={{ display: "flex" }}>
         <Toolbar className={classes.container}>
-          <Typography variant="h1" className={classes.title} component="div">
-            Aliascheck
-          </Typography>
-          <div className={classes.grow} />
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <Box sx={{ display: "flex", flex: 2 }}>
+            <Typography variant="h1" className={classes.title} component="div">
+              Aliascheck
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: { xs: "none", sm: "block" },
+              flex: 0.5,
+            }}
+          >
             {menuItems.map((item) => (
-              <Button key={item} sx={{ color: "#031521", fontWeight: 700 }}>
+              <Button
+                // className={classes.btn}
+                key={item}
+                sx={{ fontWeight: 700 }}
+              >
                 {item.toLowerCase()}
               </Button>
             ))}
             <Button
+              className={classes.button}
               variant="contained"
-              color="primary"
               sx={{ textTransform: "none" }}
             >
               Sign Up
             </Button>
+          </Box>
+          <Box>
+            <Switcher />
           </Box>
         </Toolbar>
       </Box>
     </>
   );
 };
-
 export default Navbar;
