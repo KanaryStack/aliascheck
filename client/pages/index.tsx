58c18b847b3d83c20eda3420e28dc8821d2dc9ce
rwambui <rosemwangi305@gmail.com>
2022-10-24 22:27:02 +0300

    minor error search-results correction

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,11 +2,10 @@ import * as React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Search from "../src/components/Search";
-import SearchResult from '../src/components/SearchResult/index';
 import Hint from "../src/components/Hint";
 import Navbar from "../src/components/Navbar";
 import Footer from "../src/components/Footer";
-import SearchResult from "../src/components/SearchResult"
+import SearchResult from "../src/components/SearchResult";
 import { UserInfoProvider } from "../src/utils/userInfoContext";
 // import { useTheme } from "next-themes";
 // import { DarkModeSwitch } from "react-toggle-dark-mode";
---
+++ client/src/components/SearchResult/styles.ts
@@ -0,0 +1,9 @@
+import { Theme } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles((theme: Theme) => ({
+  container: {},
+
+  span: {},
+
+ }))
