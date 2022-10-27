445f1b196bba90a0e3321c716a6efd70e2177471
Daniel Okobia <duokobia@gmail.com>
2022-10-27 22:45:15 +0100

    Create Responsiveness in Info Article  Section

--- client/src/components/intro/Intro.tsx
+++ client/src/components/intro/Intro.tsx
@@ -1,53 +1,38 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-// import { motion } from "framer-motion";
 import Image from "next/image";
 import useStyles from "./styles";
 
 function Intro() {
   const theme = useTheme();
-  // console.log(theme.palette.mode);
   const background = theme.palette.background;
-  // console.log(background);
 
   const classes = useStyles();
   return (
     <Box className={classes.container}>
       <Box className={classes.outerBox}>
-        <Box sx={{ p: 4 }}>
+        <Box className={classes.outerBox_content} sx={{ px: 8}}>
           <Typography variant="h1" className={classes.hero} sx={{ m: 1 }}>
             Finding unique usernames is
             <span className={classes.hero_fun}>fun</span> with{" "}
             <span style={{ fontWeight: "bolder" }}>Aliascheck</span>
           </Typography>
-          <Typography sx={{ fontSize: "1.4rem", textAlign: "center", p: 1 }}>
-            We help you find find unique usernames early so that you can claim
+          <Typography className={classes.hero_article}  sx={{ textAlign: "center" }}>
+            We help you find unique usernames early so that you can claim
             them. We understand the need of wanting to stand out on social media
-            with usernames and how fustraing finding a unique one can be
+            with usernames and how frustrating finding a unique one can be.
           </Typography>
         </Box>
       </Box>
-      {/* <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: { opacity: 0, scale: 0.5 },
-          visible: {
-            opacity: 1,
-            transition: { delay: 0.6, ease: "circInOut" },
-          },
-        }}
-      > */}
       <Box className={classes.bottom}>
         <Image
           src="/intro/yellow.svg"
-          alt="botto yellow"
+          alt="bottom yellow"
           width="275px"
           height="80px"
         />
       </Box>
-      {/* </motion.div> */}
       <Box className={classes.topL}>
         <Image
           src="/intro/left.svg"
