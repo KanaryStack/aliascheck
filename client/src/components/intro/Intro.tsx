a3eff74ed46741da85ca39f508a43320ce88b585
EngrZaks <abzakariyya@gmail.com>
2022-11-09 21:19:48 +0100

    fixed the intro component responsiveness

--- client/src/components/intro/Intro.tsx
+++ client/src/components/intro/Intro.tsx
@@ -1,27 +1,31 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import Image from "next/image";
 import useStyles from "./styles";
 
 function Intro() {
   const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.up("lg"));
   const background = theme.palette.background;
 
   const classes = useStyles();
   return (
     <Box className={classes.container}>
       <Box className={classes.outerBox}>
-        <Box className={classes.outerBox_content} sx={{ px: 8}}>
+        <Box className={classes.outerBox_content} sx={{ px: 8 }}>
           <Typography variant="h1" className={classes.hero} sx={{ m: 1 }}>
             Finding unique usernames is
-            <span className={classes.hero_fun}>fun</span> with{" "}
+            <span className={classes.hero_fun}>@fun</span> with{" "}
             <span style={{ fontWeight: "bolder" }}>Aliascheck</span>
           </Typography>
-          <Typography className={classes.hero_article}  sx={{ textAlign: "center" }}>
-            We help you find unique usernames early so that you can claim
-            them. We understand the need of wanting to stand out on social media
-            with usernames and how frustrating finding a unique one can be.
+          <Typography
+            className={classes.hero_article}
+            sx={{ textAlign: "center" }}
+          >
+            We help you find unique usernames early so that you can claim them.
+            We understand the need of wanting to stand out on social media with
+            usernames and how frustrating finding a unique one can be.
           </Typography>
         </Box>
       </Box>
@@ -49,16 +53,21 @@ function Intro() {
           height="90px"
         />
       </Box>
-      <Box
-        className={classes.topBox}
-        sx={{
-          background:
-            theme.palette.mode == "light" ? background.default : "black",
-        }}
-      ></Box>
-      <Box className={classes.leftBox}></Box>
-      <Box className={classes.rightBox1}></Box>
-      <Box className={classes.rightBox2}></Box>
+
+      {isMobile && (
+        <>
+          <Box
+            className={classes.topBox}
+            sx={{
+              background:
+                theme.palette.mode == "light" ? background.default : "black",
+            }}
+          ></Box>
+          <Box className={classes.leftBox}></Box>
+          <Box className={classes.rightBox1}></Box>
+          <Box className={classes.rightBox2}></Box>
+        </>
+      )}
     </Box>
   );
 }
--- client/src/components/intro/styles.ts
+++ client/src/components/intro/styles.ts
@@ -6,8 +6,7 @@ const top_margin = 10,
   width = "75%",
   height = "90%",
   border = "3px dashed #666";
-export default makeStyles((theme:Theme ) => ({
-
+export default makeStyles((theme: Theme) => ({
   container: {
     borderBottom: "1px solid black",
     position: "relative",
@@ -17,32 +16,21 @@ export default makeStyles((theme:Theme ) => ({
     margin: `${top_margin}px 0`,
     marginBottom: `${5 * top_margin}px`,
 
-    [theme.breakpoints.down(1389)]:{
-      height:'55vh',
+    [theme.breakpoints.down(1389)]: {
+      height: "55vh",
     },
-    [theme.breakpoints.down(949)]:{
-      height:'65vh',
-    }, 
-    [theme.breakpoints.down(727)]:{
-      height:'70vh',
-    },  
-    [theme.breakpoints.down(573)]:{
-      height:'80vh',
-    }, 
-    [theme.breakpoints.down(458)]:{
-      height:'85vh',
-    }, 
-    [theme.breakpoints.down(425)]:{
-      height:'90vh',
-    }, 
-    [theme.breakpoints.down(365)]:{
-      height:'95vh',
-    }, 
-    [theme.breakpoints.down(320)]:{
-      height:'100vh',
-    }, 
+    [theme.breakpoints.down(949)]: {
+      height: "65vh",
+    },
+    [theme.breakpoints.down(727)]: {
+      height: "70vh",
+    },
+    [theme.breakpoints.down(573)]: {
+      // height: "80vh",
+      borderBottom: "none",
+    },
   },
-  
+
   outerBox: {
     border,
     width,
@@ -51,87 +39,80 @@ export default makeStyles((theme:Theme ) => ({
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
+    [theme.breakpoints.down("md")]: {
+      border: "none",
+    },
   },
-  outerBox_content:{
+  outerBox_content: {
     display: "flex",
     flexDirection: "column",
     justifyItems: "center",
     alignItems: "center",
     paddingTop: "32px",
-    paddingLeft: "195px",
-    paddingRight: "195px",
 
-    [theme.breakpoints.down(1465)]:{
-      paddingLeft: "150px",
-      paddingRight: "150px",
+    [theme.breakpoints.down(1389)]: {
+      // paddingLeft: "100px",
+      // paddingRight: "100px",
     },
-    [theme.breakpoints.down(1389)]:{
-      paddingLeft: "100px",
-      paddingRight: "100px",
-    },
-    [theme.breakpoints.down(1208)]:{
+    [theme.breakpoints.down(1208)]: {
       paddingLeft: "75px",
       paddingRight: "75px",
     },
-    [theme.breakpoints.down(1141)]:{
+    [theme.breakpoints.down(1141)]: {
       paddingLeft: "50px",
       paddingRight: "50px",
     },
-    [theme.breakpoints.down(727)]:{
+    [theme.breakpoints.down(727)]: {
       paddingLeft: "40px",
       paddingRight: "40px",
     },
-    [theme.breakpoints.down(649)]:{
+    [theme.breakpoints.down(649)]: {
       paddingTop: "1rem",
-    }, 
-    [theme.breakpoints.down(425)]:{
+    },
+    [theme.breakpoints.down(425)]: {
       paddingTop: "0.5rem",
-    }, 
+    },
   },
   hero: {
     textAlign: "center",
     fontWeight: "normal",
-    fontSize: "3.25rem",
-    paddingTop: "1.5rem",
-    paddingBottom: "1.5rem",
+    fontSize: "2.25rem",
+    paddingTop: "1rem",
+    paddingBottom: "1rem",
 
-    [theme.breakpoints.down(1208)]:{
+    [theme.breakpoints.down(1208)]: {
       paddingTop: "1rem",
-      paddingBottom: "1rem"
+      paddingBottom: "1rem",
     },
-    [theme.breakpoints.down(1141)]:{
+    [theme.breakpoints.down(1141)]: {
       paddingTop: "1.5rem",
     },
-    [theme.breakpoints.down(649)]:{
+    [theme.breakpoints.down(649)]: {
       paddingTop: "1rem",
-    }, 
-    [theme.breakpoints.down(365)]:{
+    },
+    [theme.breakpoints.down(365)]: {
       paddingTop: "0.15rem",
-    }, 
-
-
-
-
+    },
   },
   hero_fun: {
     color: "#FAB535",
     margin: "0 2px",
-    marginLeft: "4px",
+    marginLeft: ".7rem",
   },
   hero_article: {
-    fontSize: "1.4rem",
+    fontSize: "1.2rem",
     p: 5,
 
-    [theme.breakpoints.between( 728, 'md')]:{
+    [theme.breakpoints.between(728, "md")]: {
       fontSize: "1.22rem",
     },
-    [theme.breakpoints.down(642)]:{
+    [theme.breakpoints.down(642)]: {
       fontSize: "1.1rem",
-      p: 0
+      p: 0,
     },
-    [theme.breakpoints.down('sm')]:{
+    [theme.breakpoints.down("sm")]: {
       fontSize: "1.125rem",
-      p: 0
+      p: 0,
     },
   },
   topBox: {
@@ -182,18 +163,21 @@ export default makeStyles((theme:Theme ) => ({
     top: "calc(100% - 80px)",
     left: "47%",
     transform: "translateX(-50%)",
+    [theme.breakpoints.down("lg")]: {
+      display: "none",
+    },
   },
   topL: {
     position: "absolute",
-    [theme.breakpoints.down('lg')]:{
-      display: 'none',
-    }
+    [theme.breakpoints.down("lg")]: {
+      display: "none",
+    },
   },
   topR: {
     position: "absolute",
     right: "0",
-    [theme.breakpoints.down('lg')]:{
-      display: 'none',
-    }
+    [theme.breakpoints.down("lg")]: {
+      display: "none",
+    },
   },
 }));
